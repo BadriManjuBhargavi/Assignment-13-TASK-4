537d46be3421245d6746864949cdaf7cb76a7015
manju12345 <manju@gmail.com>
2023-07-22 17:14:44 +0530

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace BasicCSharpAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is odd.");
+            }
+        }
+    }
+}
+
